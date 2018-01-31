b88eec1deb047dd56afc60cc720ce776435a4549
mrpuer-mini <vova@mytnik.net>
2018-01-31 22:40:48 +0300

    add tests

---
+++ e2e-tests/protractor.conf.js
@@ -0,0 +1,27 @@
+//jshint strict: false
+exports.config = {
+
+  allScriptsTimeout: 11000,
+
+  specs: [
+    '*.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  baseUrl: 'http://localhost:8000/',
+
+  framework: 'mocha',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  },
+
+  onPrepare: () => {
+    const chai = require('chai');
+    chai.use(require('chai-as-promised'));
+  }
+
+};
---
+++ e2e-tests/scenarios.js
@@ -0,0 +1,86 @@
+'use strict';
+const expect = require('chai').expect;
+/* https://github.com/angular/protractor/blob/master/docs/toc.md */
+
+describe('my app', () => {
+
+  describe('Main menu', () => {
+    let listMenuAll;
+    before(() => {
+      browser.get('/#!/list');
+      listMenuAll = element.all(by.repeater('page in $ctrl.pages'));
+    });
+
+    it('HAS a active menu element highligth', () => {
+      const listMenuFirst = listMenuAll.first().getAttribute('class');
+      expect(listMenuFirst).eventually.to.include('active');
+    });
+    it('HAS NOT inactive menu element highligth', () => {
+      const listMenuFirst = listMenuAll.last().getAttribute('class');
+      expect(listMenuFirst).eventually.to.not.include('active');
+    });
+  });
+
+  describe('/myaccount', () => {
+    before(() => {
+      browser.get('/#!/myaccount');
+    });
+
+    // it('Form is exist and visible', () => {
+    //   const userInput = element(by.model('$ctrl.newUser.name'));
+    //   expect(userInput.isDisplayed()).eventually.to.be.true;
+    // });
+
+    describe('Check for required fields', () => {
+      // let formButton;
+      // beforeEach(() => {
+      //   formButton = element(by.id('formButton'));
+      // });
+      // it('inactive button if no input text', () => {
+      //   expect(formButton.isEnabled()).eventually.to.be.false;
+      // });
+      // it('inactive button if the form is fillet not correctly', () => {
+      //   element(by.model('$ctrl.newUser.name')).sendKeys('bebe');
+      //   element(by.model('$ctrl.newUser.email')).sendKeys('a@a');
+      //   element(by.model('$ctrl.newUser.phone')).sendKeys('+799');
+      //   expect(formButton.isEnabled()).eventually.to.be.false;
+      // });
+      it('active button if form fields is submitted', () => {
+        element(by.model('$ctrl.newUser.name')).sendKeys('vova');
+        // element(by.model('$ctrl.newUser.email')).sendKeys('a@a.ru');
+        // element(by.model('$ctrl.newUser.phone')).sendKeys('+79991112233');
+        // expect(formButton.isEnabled()).eventually.to.be.true;
+      });
+    });
+  });
+
+
+  // describe('view1', function() {
+
+  //   beforeEach(function() {
+  //     browser.get('index.html#!/view1');
+  //   });
+
+
+  //   it('should render view1 when user navigates to /view1', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 1/);
+  //   });
+
+  // });
+
+
+  // describe('view2', function() {
+
+  //   beforeEach(function() {
+  //     browser.get('index.html#!/view2');
+  //   });
+
+
+  //   it('should render view2 when user navigates to /view2', function() {
+  //     expect(element.all(by.css('[ng-view] p')).first().getText()).
+  //       toMatch(/partial for view 2/);
+  //   });
+
+  // });
+});
